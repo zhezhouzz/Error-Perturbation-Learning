096a3f953f451a3d8ca64233c8e923ef35206124
zhouzhezz <zorichzorich@gmail.com>
2022-04-08 23:13:54 -0400

    fix

--- data/client/batchedq/assertion1.ml
+++ data/client/batchedq/assertion1.ml
@@ -5,14 +5,14 @@ let op_pool = [| "theta_list"; "theta_int" |]
 let libs = [| "Batchedq" |]
 
 (* let i_err = ([ -7 ], [ -5; 6; 1; 0; 8 ]) *)
-let i_err = ([ -7; -6 ], [ -5 ])
+let i_err = ([ -7; -6 ], [ -5; 6 ])
 
 let sampling_rounds = 6
 
 let p_size = 4
 
 let pre (f : Batchedq.t) (r : Batchedq.t) =
-  uniq f && uniq r && (not (empty f)) && not (empty r)
+  uniq f && uniq r && (not (empty f)) && not (size1 r)
 
 let post (f : Batchedq.t) (r : Batchedq.t) (f' : Batchedq.t) (r' : Batchedq.t)
     (u : int) =
