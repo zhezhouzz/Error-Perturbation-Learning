8720b4c1aef701d806f2a91ff881d878d9d68313
zhouzhezz <zorichzorich@gmail.com>
2022-04-12 23:13:54 -0400

    update

--- data/client/uniquel/assertion1.ml
+++ data/client/uniquel/assertion1.ml
@@ -14,7 +14,7 @@ let sampling_rounds = 6
 let p_size = 4
 
 let pre (x : int) (l1 : Uniquel.t) (l2 : Uniquel.t) (u : int) =
-  implies (mem l1 u) (once l1 u)
+  (not (size1 l1)) && implies (mem l1 u) (once l1 u)
 
 let post (x : int) (l1 : Uniquel.t) (l2 : Uniquel.t) (u : int) =
   implies (mem l2 u) (once l2 u) && iff (mem l2 u) (mem l1 u || u == x)
